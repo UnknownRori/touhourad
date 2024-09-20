bdb9dc6bee9491fef044c1878189baf76f3b8345
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-09-20 12:26:01 +0700

    📦 initial commit

---
+++ android/app/src/main/kotlin/com/example/touhourad/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.touhourad
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:touhourad/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
